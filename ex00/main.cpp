1a551d71d42be8f413a4a01cfd4772f14b810370
Mateo Corso <mcorso@student.42.fr>
2023-02-15 14:28:57 +0100

    Protecting main function

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nikoraxx <nikoraxx@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 20:38:02 by nikoraxx          #+#    #+#             */
-/*   Updated: 2023/02/13 17:07:05 by nikoraxx         ###   ########.fr       */
+/*   Updated: 2023/02/15 14:27:59 by nikoraxx         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@
 
 int	main(int argc, const char **argv)
 {
-	(void)argc;
+	if (argc != 2)
+	{
+		std::cout << "Please, provide a single argument\n";
+		return (1);
+	}
 	std::string	definition = argv[1];
 	Literal literal(definition);
 
